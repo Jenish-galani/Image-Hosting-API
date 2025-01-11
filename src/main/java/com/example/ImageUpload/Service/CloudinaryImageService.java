a2fe6f67d06e31880cbd0d446e960a6940176dee
Jenish Galani <jenishgalani4543@gmail.com>
2025-01-11 13:29:10 +0530

    Update CloudinaryImageService.java
    .

--- src/main/java/com/example/ImageUpload/Service/CloudinaryImageService.java
+++ src/main/java/com/example/ImageUpload/Service/CloudinaryImageService.java
@@ -5,6 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.Map;
 
 public interface CloudinaryImageService {
+    // 📂 Method to handle image uploads to Cloudinary
     public Map upload(MultipartFile file);
 }
-
